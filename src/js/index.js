fb404cb4df61e7c33b7f021e9ff12809a51914bd
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2025-08-05 09:58:43 -0400

    reformat the recipe

--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,25 @@ const showRecipe = async function () {
 	try {
 		// console.log("??");
 		const resp = await fetch(
-			"https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886"
+			"https://forkify-api.jonas.io/api/v2/recipes/664c8f193e7aa067e94e8355"
 		);
 		// console.log(resp);
 		if (!resp.ok) throw new Error(`🐛 💥 ${resp.message} (${resp.status})`);
 		const data = await resp.json();
 		console.log(data);
 
-		
+		let { recipe } = data.data;
+		recipe = {
+			id: recipe.id,
+			title: recipe.title,
+			publier: recipe.publier,
+			sourceUrl: recipe.source_url,
+			image: recipe.image_url,
+			servings: recipe.servings,
+			cookingTime: recipe.cooking_time,
+			ingredints: recipe.ingredints,
+		};
+    console.log(recipe);
 	} catch (err) {
 		console.error(err);
 	}
